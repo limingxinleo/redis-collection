6fab58d3fff82e6fbf2dd06f44cac6093dbe3aec
李铭昕 <715557344@qq.com>
2018-09-17 11:31:30 +0800

    项目初始化

---
+++ tests/Cases/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+namespace SwoftTest\Cases;
+
+class ExampleTest extends AbstractTestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
