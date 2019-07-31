d337967da2150492c43c054b7d5b50303b5ad31f
武杰 <1224805548@qq.com>
2019-07-31 18:08:32 +0800

    string

--- src/StringCollection.php
+++ src/StringCollection.php
@@ -55,14 +55,14 @@ abstract class StringCollection
     /**
      * 设置数据.
      * @param $id
-     * @param $value 数据
+     * @param string $value 数据
      * @param int $ttl 超时时间 秒
      * @return bool
      */
     public function set($id, $value, $ttl = 3600)
     {
         $key = $this->getCacheKey($id);
-        if (! is_null($ttl)) {
+        if (!is_null($ttl)) {
             return $this->redis()->set($key, $value, $ttl);
         }
         return $this->redis()->set($key, $value);
